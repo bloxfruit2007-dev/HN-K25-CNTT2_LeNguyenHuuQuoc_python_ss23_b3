d9436453164178f14f34449f90720799cde57ace
lenguyenhuuquoc <bloxfruit2007@gmail.com>
2026-06-15 00:58:18 +0700

    Update flights.py

---
+++ b3/data/flights.py
@@ -0,0 +1,4 @@
+flights = [
+    {"flight_id": "RA001", "passengers": 154, "depart_time": "2026-06-15 08:00:00", "duration_min": 120},
+    {"flight_id": "RA002", "passengers": 85, "depart_time": "2026-06-15 13:30:00", "duration_min": 45}
+]
